99fd8d3146579b494704cb001784ee2b8e41d8da
Prarthit <prarthitpatel1010@gmail.com>
2026-06-09 22:58:21 +0530

    using then React

---
+++ Episode 01 - Inception/App.js
@@ -0,0 +1,7 @@
+
+        const heading = React.createElement("h1",{},"This is h1 from the React");
+
+        const root =ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(heading);
+
