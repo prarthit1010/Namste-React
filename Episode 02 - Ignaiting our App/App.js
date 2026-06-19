03a9df83423dc9c21fc8c83f7b0993f5f8b97314
Prarthit <prarthitpatel1010@gmail.com>
2026-06-19 22:07:04 +0530

    Install React and Remove the CDN linkes

---
+++ Episode 02 - Ignaiting our App/App.js
@@ -0,0 +1,21 @@
+// const heading = React.createElement("h1",{ id : "heading" , xyz : "abc"},"This is h1 from the React");
+// //{} is used for the Atribute in react
+//  const root =ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+ // if we want to multiple h1 tag or w want to h1 inside h1 tag then ???
+
+ import React from "react";
+ //import ReactDOM from "react-dom"
+ import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div",{ id : "Parent"},
+        React.createElement("div",{id : "child"},
+                [ React.createElement("h1",{key :1},"This is h1 from the React"),
+                React.createElement("h2",{key :2},"This is h2 from the React")]));
+                
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
