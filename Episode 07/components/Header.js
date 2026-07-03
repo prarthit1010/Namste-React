423ac30e4ea393f0453a1953629bc48f2a1bcfe9
Prarthit <prarthitpatel1010@gmail.com>
2026-07-03 18:00:08 +0530

    Use <Link></Link> Tag Insted of <a> tag

--- Episode 07/components/Header.js
+++ Episode 07/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";  
+import { Link } from "react-router";
   
   const Header = () =>{
 
@@ -10,8 +11,10 @@ import { LOGO_URL } from "../utils/constant";
         <img  className="header-logo" src={LOGO_URL}/>
     
           <ul>
-            <li>About us</li>
-            <li>Contect us</li>
+        {/** Never use <a></a> anchor tag in react beacuse when w euse anchor tag then it reload ghe page and render the hole page isted of use Link tag which is provided by the react-router it is not render the hole page isted of render the only chnage dthing in this this when we click on the about page thn not load header but only load the about componenet and 
+        isted of contact us oage load the hole page so never us ethe anchor<a></a> Tag in React */}
+            <li><Link to= "/about">About us</Link></li>
+            <li><a href="/contactus">Contect us</a></li>
             <li>Home</li>
             <li>Our info</li>
             <button className="login-btn" onClick={() =>{lgnbtn == "Login" ? setlgnbtn("Logout") : setlgnbtn("Login")}}>{lgnbtn}</button>
