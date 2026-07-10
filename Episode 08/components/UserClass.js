87fca492ceac5ce2d27992cfe78b555e24bdf019
Prarthit <prarthitpatel1010@gmail.com>
2026-07-10 13:53:48 +0530

    Stat varible in classbase components

--- Episode 08/components/UserClass.js
+++ Episode 08/components/UserClass.js
@@ -4,13 +4,25 @@ class UserClass extends React.Component{
 
     constructor(props){
         super(props);
+
+        this.state = {
+            count : 0,
+            count2 : 3
+        }
     }
 
     render(){
 
+        const {count , count2} = this.state;
+        const {name} = this.props;
+
         return(
         <div className='user'>
-            <h1>Name : {this.props.name}</h1>
+
+            <h1>Count = {count}</h1>
+            <h1>Count = {count2}</h1>
+
+            <h2>Name : {name}</h2>
             <h2>This Course is namste React</h2>
         </div>
         )
